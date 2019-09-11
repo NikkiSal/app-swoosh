42b072ada959cab7296efb4e084fa86d9bb81a47
Nikki <Nikkisali9@gmail.com>
2019-09-11 10:42:01 -0600

    Intro to segues

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -20,9 +20,7 @@ class ViewController: UIViewController {
             }
 
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSKillVC(unwindSegue: UIStoryboardSegue) {
     }
 
 
