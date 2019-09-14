3aca57b19d9bc458686e936239951e2b179d6c85
Nikki <Nikkisali9@gmail.com>
2019-09-13 18:16:29 -0600

    handling events and data models

--- app-swoosh/app-swoosh/LeagueVC.swift
+++ app-swoosh/app-swoosh/LeagueVC.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player: Player!
 
         override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player ()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
-        
+       
+    }
+    
+    @IBAction func onMenstapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+         selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+         selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
 
---
+++ app-swoosh/app-swoosh/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Marzieh on 2019-09-13.
+//  Copyright © 2019 Myph. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague : String?
+    var selectedSkillLevel: String?
+}
